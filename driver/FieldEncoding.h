902283fde36c9a64b3b9f7be40feec46c4cc2d7b
daniel-ames <daniel.ames@hp.com>
2021-02-04 17:14:38 -0600

    Day3: Added comments to show which vmcs encoding fields pertain to registers.

--- driver/FieldEncoding.h
+++ driver/FieldEncoding.h
@@ -33,9 +33,9 @@ enum __vmcs_width_e {
 enum __vmcs_encoding_e {
 
 	// Control 16-bit
-	VPID = VMCS_ENCODE_COMPONENT_FULL_16(control, 0),
+	Vpid = VMCS_ENCODE_COMPONENT_FULL_16(control, 0),
 	PostInterruptNotificationVector = VMCS_ENCODE_COMPONENT_FULL_16(control, 1),
-	ETPTIndex= VMCS_ENCODE_COMPONENT_FULL_16(control, 2),
+	EtptIndex= VMCS_ENCODE_COMPONENT_FULL_16(control, 2),
 
 	// Control 32-bit
 	PinBasedVmExecutionControls = VMCS_ENCODE_COMPONENT_FULL_32(control, 0),
@@ -98,114 +98,114 @@ enum __vmcs_encoding_e {
 	Cr3TargetValue3 = VMCS_ENCODE_COMPONENT_FULL(control, natural, 7),
 
 	// Guest 16-bit
-	GuestEsSelector = VMCS_ENCODE_COMPONENT_FULL_16(guest, 0),
-	GuestCsSelector = VMCS_ENCODE_COMPONENT_FULL_16(guest, 1),
-	GuestSsSelector = VMCS_ENCODE_COMPONENT_FULL_16(guest, 2),
-	GuestDsSelector = VMCS_ENCODE_COMPONENT_FULL_16(guest, 3),
-	GuestFsSelector = VMCS_ENCODE_COMPONENT_FULL_16(guest, 4),
-	GuestGsSelector = VMCS_ENCODE_COMPONENT_FULL_16(guest, 5),
-	GuestLdtrSelector = VMCS_ENCODE_COMPONENT_FULL_16(guest, 6),
-	GuestTrSelector = VMCS_ENCODE_COMPONENT_FULL_16(guest, 7),
+	GuestEsSelector = VMCS_ENCODE_COMPONENT_FULL_16(guest, 0), // register
+	GuestCsSelector = VMCS_ENCODE_COMPONENT_FULL_16(guest, 1), // register
+	GuestSsSelector = VMCS_ENCODE_COMPONENT_FULL_16(guest, 2), // register
+	GuestDsSelector = VMCS_ENCODE_COMPONENT_FULL_16(guest, 3), // register
+	GuestFsSelector = VMCS_ENCODE_COMPONENT_FULL_16(guest, 4), // register
+	GuestGsSelector = VMCS_ENCODE_COMPONENT_FULL_16(guest, 5), // register
+	GuestLdtrSelector = VMCS_ENCODE_COMPONENT_FULL_16(guest, 6), // register
+	GuestTrSelector = VMCS_ENCODE_COMPONENT_FULL_16(guest, 7), // register
 	GuestInterruptStatus = VMCS_ENCODE_COMPONENT_FULL_16(guest, 8),
 	PmlIndex = VMCS_ENCODE_COMPONENT_FULL_16(guest, 9),
 
 	// Guest 32-bit
-	GuestEsLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 0),
-	GuestCsLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 1),
-	GuestSsLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 2),
-	GuestDsLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 3),
-	GuestFsLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 4),
-	GuestGsLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 5),
-	GuestLdtrLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 6),
-	GuestTrLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 7),
-	GuestGdtrLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 8),
-	GuestIdtrLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 9),
-	GuestEsAccessRights = VMCS_ENCODE_COMPONENT_FULL_32(guest, 10),
-	GuestCsAccessRights = VMCS_ENCODE_COMPONENT_FULL_32(guest, 11),
-	GuestSsAccessRights = VMCS_ENCODE_COMPONENT_FULL_32(guest, 12),
-	GuestDsAccessRights = VMCS_ENCODE_COMPONENT_FULL_32(guest, 13),
-	GuestFsAccessRights = VMCS_ENCODE_COMPONENT_FULL_32(guest, 14),
-	GuestGsAccessRights = VMCS_ENCODE_COMPONENT_FULL_32(guest, 15),
-	GuestLdtrAccessRights = VMCS_ENCODE_COMPONENT_FULL_32(guest, 16),
-	GuestTrAccessRights = VMCS_ENCODE_COMPONENT_FULL_32(guest, 17),
+	GuestEsLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 0), // register
+	GuestCsLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 1), // register
+	GuestSsLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 2), // register
+	GuestDsLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 3), // register
+	GuestFsLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 4), // register
+	GuestGsLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 5), // register
+	GuestLdtrLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 6), // register
+	GuestTrLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 7), // register
+	GuestGdtrLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 8), // register
+	GuestIdtrLimit = VMCS_ENCODE_COMPONENT_FULL_32(guest, 9), // register
+	GuestEsAccessRights = VMCS_ENCODE_COMPONENT_FULL_32(guest, 10), // register
+	GuestCsAccessRights = VMCS_ENCODE_COMPONENT_FULL_32(guest, 11), // register
+	GuestSsAccessRights = VMCS_ENCODE_COMPONENT_FULL_32(guest, 12), // register
+	GuestDsAccessRights = VMCS_ENCODE_COMPONENT_FULL_32(guest, 13), // register
+	GuestFsAccessRights = VMCS_ENCODE_COMPONENT_FULL_32(guest, 14), // register
+	GuestGsAccessRights = VMCS_ENCODE_COMPONENT_FULL_32(guest, 15), // register
+	GuestLdtrAccessRights = VMCS_ENCODE_COMPONENT_FULL_32(guest, 16), // register
+	GuestTrAccessRights = VMCS_ENCODE_COMPONENT_FULL_32(guest, 17), // register
 	GuestInterruptibilityState = VMCS_ENCODE_COMPONENT_FULL_32(guest, 18),
 	GuestActivityState = VMCS_ENCODE_COMPONENT_FULL_32(guest, 19),
-	GuestSmbase = VMCS_ENCODE_COMPONENT_FULL_32(guest, 20),
-	GuestIa32SysenterCs = VMCS_ENCODE_COMPONENT_FULL_32(guest, 21),
+	GuestSmbase = VMCS_ENCODE_COMPONENT_FULL_32(guest, 20), // register
+	GuestIa32SysenterCs = VMCS_ENCODE_COMPONENT_FULL_32(guest, 21), // register
 		//////////////////////////////////////////// wait... where's 22?? <--------------------
 	VmxPreemptionTimerValue = VMCS_ENCODE_COMPONENT_FULL_32(guest, 23),
 
 	// Guest 64-bit
-	VmcsLinkpointer = VMCS_ENCODE_COMPONENT_FULL_64(guest, 0),
-	GuestIa32Debugctl = VMCS_ENCODE_COMPONENT_FULL_64(guest, 1),
-	GuestIa32Pat = VMCS_ENCODE_COMPONENT_FULL_64(guest, 2),
-	GuestIa32Efer = VMCS_ENCODE_COMPONENT_FULL_64(guest, 3),
-	GuestPerfGlobalCtrl = VMCS_ENCODE_COMPONENT_FULL_64(guest, 4),
+	VmcsLinkpointer = VMCS_ENCODE_COMPONENT_FULL_64(guest, 0), // register
+	GuestIa32Debugctl = VMCS_ENCODE_COMPONENT_FULL_64(guest, 1), // register
+	GuestIa32Pat = VMCS_ENCODE_COMPONENT_FULL_64(guest, 2), // register
+	GuestIa32Efer = VMCS_ENCODE_COMPONENT_FULL_64(guest, 3), // register
+	GuestPerfGlobalCtrl = VMCS_ENCODE_COMPONENT_FULL_64(guest, 4), // register
 	GuestPdpte0 = VMCS_ENCODE_COMPONENT_FULL_64(guest, 5),
 	GuestPdpte1 = VMCS_ENCODE_COMPONENT_FULL_64(guest, 6),
 	GuestPdpte2 = VMCS_ENCODE_COMPONENT_FULL_64(guest, 7),
 	GuestPdpte3 = VMCS_ENCODE_COMPONENT_FULL_64(guest, 8),
-	GuestIa32Bindcfgs = VMCS_ENCODE_COMPONENT_FULL_64(guest, 9),
+	GuestIa32Bindcfgs = VMCS_ENCODE_COMPONENT_FULL_64(guest, 9), // register
 	GuestIa32RtitCtl = VMCS_ENCODE_COMPONENT_FULL_64(guest, 10),
 		//////////////////////////////////////////// wait... where's 11?? <--------------------
 	GuestIa32Pkrs = VMCS_ENCODE_COMPONENT_FULL_64(guest, 12),
 
 	// Guest natural width
-	GuestCr0 = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 0),
-	GuestCr3 = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 1),
-	GuestCr4 = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 2),
-	GuestEsBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 3),
-	GuestCsBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 4),
-	GuestSsBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 5),
-	GuestDsBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 6),
-	GuestFsBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 7),
-	GuestGsBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 8),
-	GuestLdtrBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 9),
-	GuestTrBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 10),
-	GuestGdtrBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 11),
-	GuestIdtrBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 12),
-	GuestDr7 = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 13),
-	GuestRsp = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 14),
-	GuestRip = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 15),
-	GuestRflags = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 16),
+	GuestCr0 = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 0), // register
+	GuestCr3 = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 1), // register
+	GuestCr4 = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 2), // register
+	GuestEsBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 3), // register
+	GuestCsBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 4), // register
+	GuestSsBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 5), // register
+	GuestDsBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 6), // register
+	GuestFsBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 7), // register
+	GuestGsBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 8), // register
+	GuestLdtrBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 9), // register
+	GuestTrBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 10), // register
+	GuestGdtrBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 11), // register
+	GuestIdtrBase = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 12), // register
+	GuestDr7 = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 13), // register
+	GuestRsp = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 14), // register
+	GuestRip = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 15), // register
+	GuestRflags = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 16), // register
 	GuestPendingDebugExceptions = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 17),
-	GuestIa32SysenterEsp = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 18),
-	GuestIa32SysenterEip = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 19),
+	GuestIa32SysenterEsp = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 18), // register
+	GuestIa32SysenterEip = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 19), // register
 	GuestIa32SCet = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 20),
 	GuestSsp = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 21),
 	GuestIa32InterruptSspTableAddr = VMCS_ENCODE_COMPONENT_FULL(guest, natural, 22),
 
 	// Host 16-bit
-	HostEsSelector = VMCS_ENCODE_COMPONENT_FULL_16(host, 0),
-	HostCsSelector = VMCS_ENCODE_COMPONENT_FULL_16(host, 1),
-	HostSsSelector = VMCS_ENCODE_COMPONENT_FULL_16(host, 2),
-	HostDsSelector = VMCS_ENCODE_COMPONENT_FULL_16(host, 3),
-	HostFsSelector = VMCS_ENCODE_COMPONENT_FULL_16(host, 4),
-	HostGsSelector = VMCS_ENCODE_COMPONENT_FULL_16(host, 5),
-	HostTrSelector = VMCS_ENCODE_COMPONENT_FULL_16(host, 6),
+	HostEsSelector = VMCS_ENCODE_COMPONENT_FULL_16(host, 0), // register
+	HostCsSelector = VMCS_ENCODE_COMPONENT_FULL_16(host, 1), // register
+	HostSsSelector = VMCS_ENCODE_COMPONENT_FULL_16(host, 2), // register
+	HostDsSelector = VMCS_ENCODE_COMPONENT_FULL_16(host, 3), // register
+	HostFsSelector = VMCS_ENCODE_COMPONENT_FULL_16(host, 4), // register
+	HostGsSelector = VMCS_ENCODE_COMPONENT_FULL_16(host, 5), // register
+	HostTrSelector = VMCS_ENCODE_COMPONENT_FULL_16(host, 6), // register
 
 	// Host 32-bit
-	HostIa32SysenterCs = VMCS_ENCODE_COMPONENT_FULL_32(host, 0),
+	HostIa32SysenterCs = VMCS_ENCODE_COMPONENT_FULL_32(host, 0), // register
 
 	// Host 64-bit
-	HostIa32Pat = VMCS_ENCODE_COMPONENT_FULL_64(host, 0),
-	HostIa32Efer = VMCS_ENCODE_COMPONENT_FULL_64(host, 1),
-	HostPerfGlobalCtrl = VMCS_ENCODE_COMPONENT_FULL_64(host, 2),
+	HostIa32Pat = VMCS_ENCODE_COMPONENT_FULL_64(host, 0), // register
+	HostIa32Efer = VMCS_ENCODE_COMPONENT_FULL_64(host, 1), // register
+	HostPerfGlobalCtrl = VMCS_ENCODE_COMPONENT_FULL_64(host, 2), // register
 	HostIa32Pkrs = VMCS_ENCODE_COMPONENT_FULL_64(host, 3),
 
 	// Host natural width
-	HostCr0 = VMCS_ENCODE_COMPONENT_FULL(host, natural, 0),
-	HostCr3 = VMCS_ENCODE_COMPONENT_FULL(host, natural, 1),
-	HostCr4 = VMCS_ENCODE_COMPONENT_FULL(host, natural, 2),
-	HostFsBase = VMCS_ENCODE_COMPONENT_FULL(host, natural, 3),
-	HostGsBase = VMCS_ENCODE_COMPONENT_FULL(host, natural, 4),
-	HostTrBase = VMCS_ENCODE_COMPONENT_FULL(host, natural, 5),
-	HostGdtrBase = VMCS_ENCODE_COMPONENT_FULL(host, natural, 6),
-	HostIdtrBase = VMCS_ENCODE_COMPONENT_FULL(host, natural, 7),
-	HostIa32SysenterEsp = VMCS_ENCODE_COMPONENT_FULL(host, natural, 8),
-	HostIa32SysenterEip = VMCS_ENCODE_COMPONENT_FULL(host, natural, 9),
-	HostRsp = VMCS_ENCODE_COMPONENT_FULL(host, natural, 10),
-	HostRip = VMCS_ENCODE_COMPONENT_FULL(host, natural, 11),
+	HostCr0 = VMCS_ENCODE_COMPONENT_FULL(host, natural, 0), // register
+	HostCr3 = VMCS_ENCODE_COMPONENT_FULL(host, natural, 1), // register
+	HostCr4 = VMCS_ENCODE_COMPONENT_FULL(host, natural, 2), // register
+	HostFsBase = VMCS_ENCODE_COMPONENT_FULL(host, natural, 3), // register
+	HostGsBase = VMCS_ENCODE_COMPONENT_FULL(host, natural, 4), // register
+	HostTrBase = VMCS_ENCODE_COMPONENT_FULL(host, natural, 5), // register
+	HostGdtrBase = VMCS_ENCODE_COMPONENT_FULL(host, natural, 6), // register
+	HostIdtrBase = VMCS_ENCODE_COMPONENT_FULL(host, natural, 7), // register
+	HostIa32SysenterEsp = VMCS_ENCODE_COMPONENT_FULL(host, natural, 8), // register
+	HostIa32SysenterEip = VMCS_ENCODE_COMPONENT_FULL(host, natural, 9), // register
+	HostRsp = VMCS_ENCODE_COMPONENT_FULL(host, natural, 10), // register
+	HostRip = VMCS_ENCODE_COMPONENT_FULL(host, natural, 11), // register
 	HostIa32SCet = VMCS_ENCODE_COMPONENT_FULL(host, natural, 12),
 	HostSsp = VMCS_ENCODE_COMPONENT_FULL(host, natural, 13),
 	HostIa32InterruptSspTableAddr = VMCS_ENCODE_COMPONENT_FULL(host, natural, 14),
